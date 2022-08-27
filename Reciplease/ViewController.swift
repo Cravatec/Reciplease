5341b076b4e57fada11f4ddfd52dc8a4b5cfcd0a
Samuel <92946129+Cravatec@users.noreply.github.com>
2022-08-27 12:22:35 +0200

    Initial Commit

---
+++ Reciplease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Sam on 27/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
