b215b5fa89fc481905ea7d000c63257d7ceda454
Samuel <92946129+Cravatec@users.noreply.github.com>
2022-09-18 17:13:34 +0200

    add Outlet in RecipesResponseViewController

--- Reciplease/Controller/RecipesResponseViewController.swift
+++ Reciplease/Controller/RecipesResponseViewController.swift
@@ -9,13 +9,19 @@ import Foundation
 import UIKit
 
 class RecipesResponseViewController: UIViewController {
+
+    @IBOutlet weak var recipesResponseTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+class RecipesResponseTableViewCell: UITableViewCell {
     @IBOutlet weak var recipeTitleLabel: UILabel!
     @IBOutlet weak var recipeTimeLabel: UILabel!
     @IBOutlet weak var recipeNoteLabel: UILabel!
     @IBOutlet weak var recipeIngredientsLabel: UILabel!
     @IBOutlet weak var recipeResponseTableView: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 }
