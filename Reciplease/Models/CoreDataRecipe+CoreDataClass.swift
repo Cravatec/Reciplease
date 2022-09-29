dac68dacb031455458bfbfab1e772b996722785b
Samuel <92946129+Cravatec@users.noreply.github.com>
2022-09-29 19:58:12 +0200

    update CoreData

---
+++ Reciplease/Models/CoreDataRecipe+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataRecipe+CoreDataClass.swift
+//  Reciplease
+//
+//  Created by Sam on 29/09/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class CoreDataRecipe: NSManagedObject {
+
+}
---
+++ Reciplease/Models/CoreDataRecipe+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  CoreDataRecipe+CoreDataProperties.swift
+//  Reciplease
+//
+//  Created by Sam on 29/09/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CoreDataRecipe {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CoreDataRecipe> {
+        return NSFetchRequest<CoreDataRecipe>(entityName: "CoreDataRecipe")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var time: Double
+    @NSManaged public var like: Double
+    @NSManaged public var image: String?
+    @NSManaged public var ingredients: String?
+    @NSManaged public var url: String?
+
+}
+
+extension CoreDataRecipe : Identifiable {
+
+}
