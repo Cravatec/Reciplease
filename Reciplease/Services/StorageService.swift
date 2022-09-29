a72f1425f75b3edd3672693d94f31cabfff74ddf
Samuel <92946129+Cravatec@users.noreply.github.com>
2022-09-29 11:35:24 +0200

    add CoreData for Favorites Recipes

---
+++ Reciplease/Services/StorageService.swift
@@ -0,0 +1,30 @@
+//
+//  StorageService.swift
+//  Reciplease
+//
+//  Created by Sam on 25/09/2022.
+//
+
+import Foundation
+import CoreData
+
+protocol RecipeStorageService {
+    func save(recipe: Recipe)
+
+    func retrieve() -> [Recipe]
+}
+
+class CoreDataRecipeStorage: RecipeStorageService {
+    
+ //   var coreData
+    
+    func save(recipe: Recipe) {
+        
+    }
+    
+    func retrieve() -> [Recipe] {
+        return []
+    }
+    
+}
+
