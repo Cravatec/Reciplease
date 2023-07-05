c22408a25fa06a70cc4c380615b4fab88e3e0716
Samuel <92946129+Cravatec@users.noreply.github.com>
2023-07-05 15:59:37 +0200

    add retrieval result completion for success and failure

--- Reciplease/Models/CoreDataStack.swift
+++ Reciplease/Models/CoreDataStack.swift
@@ -16,6 +16,48 @@ class CoreDataStack {
     private let appDelegate = UIApplication.shared.delegate as? AppDelegate
     lazy var context = appDelegate?.persistentContainer.viewContext
     
+    private enum StorageError: Error {
+        case notFound
+    }
+    
+    func retrieve(completion: (Result<[Recipe], Error>) -> Void) {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "CoreDataRecipe")
+        do {
+            guard let result = try context?.fetch(fetchRequest) as? [NSManagedObject] else {
+                return completion(.failure(StorageError.notFound))
+            }
+            var recipes = [Recipe]()
+            for data in result  {
+                let title = data.value(forKey: "title") as? String
+                let image = data.value(forKey: "image") as? String
+                let ingredientsLitteral = data.value(forKey: "ingredients") as? String
+                let ingredientsArray = ingredientsLitteral?.components(separatedBy: ",")
+                let ingredients = ingredientsArray?.compactMap({ ingredientString in
+                    RecipeIngredients(text: ingredientString, image: nil)
+                })
+                let like = data.value(forKey: "like") as? Double
+                let time = data.value(forKey: "time") as? Double
+                let url = data.value(forKey: "url") as? String
+                let imageUrl = URL(string: image ?? "")
+                let recipeUrl = URL(string: url ?? "")
+                let recipe = Recipe(title: title,
+                                    subtitle: nil,
+                                    image: imageUrl,
+                                    like: like,
+                                    time: time,
+                                    detailIngredients: nil,
+                                    uri: nil,
+                                    url: recipeUrl!,
+                                    ingredients: ingredients)
+                recipes.append(recipe)
+                print("retrieve")
+            }
+            completion(.success(recipes))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
     func retrieve() -> [Recipe] {
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "CoreDataRecipe")
         do {
