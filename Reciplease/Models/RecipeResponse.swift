4627558094c4eb2b9a50d751f65cfd427da7f450
Samuel <92946129+Cravatec@users.noreply.github.com>
2023-07-18 16:25:10 +0200

    Remove unnecessary response in RecipeResponse.swift

--- Reciplease/Models/RecipeResponse.swift
+++ Reciplease/Models/RecipeResponse.swift
@@ -43,19 +43,7 @@ struct APIRecipe: Decodable {
 
 struct RecipeIngredients: Decodable {
     let text: String
-//    let quantity: Double
-//    let measure: String?
-//    let food: String
-//    let weight: Double
-//    let foodCategory: String?
-//    let foodID: String
     let image: String?
-    
-//    enum CodingKeys: String, CodingKey {
-//        case text, quantity, measure, food, weight, foodCategory
-//        case foodID = "foodId"
-//        case image
-//    }
 }
 
 // MARK: - Images
