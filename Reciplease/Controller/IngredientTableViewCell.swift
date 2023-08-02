26bebe5b387dfa41a0ac34c13a3630be5420881b
Samuel <92946129+Cravatec@users.noreply.github.com>
2023-08-02 14:51:17 +0200

    update layout for IngredientTableViewCell

--- Reciplease/Controller/IngredientTableViewCell.swift
+++ Reciplease/Controller/IngredientTableViewCell.swift
@@ -16,19 +16,13 @@ class IngredientTableViewCell: UITableViewCell {
         if let url = url {
             ingredientImage.imageLoadingFromURL(url: url)
         }
-        ingredientImage.layer.cornerRadius = ingredientImage.frame.height / 2
         ingredientLabel.text = ingredientText
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        ingredientImage.layer.cornerRadius = 60 / 2
+        selectionStyle = .none
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
